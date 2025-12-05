f084674a0f6314a7cd70ec8d60a21d96dcf07c81
shubham kumar <clboy768@gmail.com>
2025-12-05 09:38:30 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,39 @@
+// let x = 10;
+// let y = 5;
+// console.log(x+y)
+
+// 
+
+
+// let -->
+// let x = 10;
+// let x = 5 //redeclare
+// console.log(x)  //can not redeclare
+
+// let x = 10;
+// x = 6;
+// console.log(x)
+
+
+// var x = 10;
+// var x = 9;
+// console.log(x)
+
+// var x = 10 ;
+// x = 2;
+// console.log(x)
+
+
+// const x = 10;
+// const x = 4
+// console.log(x)  //redeclaration not possible
+
+// const x = 10;
+//     x = 3
+//     console.log(x)  //can not reassign in case of const
+
+
+
+// Undefined --> it is a default value used by java script when variable is declared but its value is not assigned it will give undefined
+
+// Null --> it is a user defined value which is equal to nothing
